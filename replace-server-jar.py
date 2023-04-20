4103cd39e5791f12e1de75672607ca4045cc6d25
Neil Hemmerly <neilhemm0@gmail.com>
2023-04-20 11:15:08 -0400

    Finish untested draft

--- replace-server-jar.py
+++ replace-server-jar.py
@@ -3,9 +3,44 @@
 import os
 import sys
 import paramiko
+from scp import SCPClient
 
-def trade_server_jar(server, ssh):
+def trade_server_jar(ssh):
     command = "./~/tools/swap-delete.sh"
+    _stdin, stdout, _stderr = ssh.exec_command(command)
+    ssh.close()
 
 def scp_server_jar(server, ssh):
-    command = "scp *.jar host@192.168.2.130:~/"
+    blah = 'blah'
+
+def stop_mc_server(ssh):
+    command = 'systemctl stop minecraft.service'
+    _stdin, stdout, _stderr = ssh.exec_command(command)
+    ssh.close()
+
+def start_mc_server(ssh):
+    command = 'systemctl start minecraft.service'
+    _stdin, stdout, _stderr = ssh.exec_command(command)
+    ssh.close()
+
+def key_connect():
+    ip = "192.168.2.130"
+    account = 'minecraft'
+    key = paramiko.RSAkey.from_private_key_file('path to key file')
+    client = paramiko.SSHClient()
+    policy = paramiko.AutoAddPolicy()
+    client.set_missing_host_key_policy(policy)
+    client.connect(ip, username=account, pkey=key)
+    return client
+
+def main():
+    # Connect to server
+    connection = key_connect()
+    # Stop Minecraft
+    stop_mc_server(connection)
+    # Copy server jar to server via scp
+    scp_server_jar(connection)
+    # Replace jar file in minecraft/server/paperMC
+    trade_server_jar(connection)
+    # Start Minecraft.service
+    start_mc_server(connection)
--- swap-delete.sh
+++ swap-delete.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+rm ~/server/paperMC/*.jar
+
 mv ~/*.jar ~/server/paperMC/
 
-
-
+ls ~/server/paperMC
